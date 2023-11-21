c4468f1630790ca186f3e4b417c88cd9bbfae1ac
amit0604 <151597559+amit0604@users.noreply.github.com>
2023-11-21 05:24:31 +0200

    Add files via upload

---
+++ functoins.js
@@ -0,0 +1,10 @@
+/* מפעיל את הנביגיישן בר במסכים קטנים */
+function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+      x.className += " responsive";
+    } else {
+      x.className = "topnav";
+    }
+  }
+
